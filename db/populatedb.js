414e53d27f6909cdedc417aa06a2840f9707ed7e
21Neil <88711171+21Neil@users.noreply.github.com>
2025-06-13 17:07:43 +0800

    feat: add remote database

---
+++ db/populatedb.js
@@ -0,0 +1,22 @@
+import { Client } from 'pg';
+import 'dotenv/config';
+
+const SQL = `
+  CREATE TABLE IF NOT EXISTS message (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    text VARCHAR(255),
+    username VARCHAR(50),
+    added TIMESTAMP
+  );
+`;
+
+const main = async () => {
+  console.log('seeding...');
+  const client = new Client(process.env.DB_URL);
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log('done');
+};
+
+main()
