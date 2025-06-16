c6812f6fbf692108ac9c059e4598f53cb6a986ee
21Neil <88711171+21Neil@users.noreply.github.com>
2025-06-16 13:49:28 +0800

    feat: change db url

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,7 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === 'prod'
-      ? Postgres.DATABASE_URL
-      : process.env.DB_URL,
+  connectionString: process.env.DB_URL,
 });
 
 export default pool;
