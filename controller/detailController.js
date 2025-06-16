dd22f0db0f35bce16993296fbaabdec72d1c9e8b
21Neil <88711171+21Neil@users.noreply.github.com>
2025-06-16 13:57:20 +0800

    feat: change env var

--- controller/detailController.js
+++ controller/detailController.js
@@ -2,7 +2,6 @@ import { getMessageById } from "../db/queries.js"
 
 const getDetailView = async (req, res) => {
   const message = (await getMessageById(Number(req.params.id))).rows[0]
-  console.log(message)
   res.render('detail', { title: 'Detail', message })
 }
 
